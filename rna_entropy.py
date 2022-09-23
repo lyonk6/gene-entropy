fa034b0ce106998984fcc8d98b496a4bda23e713
Ken Lyon <fatstrat389@gmail.com>
2022-09-22 17:55:55 -0700

    Added a binplot to check the distribution of our data.

--- rna_entropy.py
+++ rna_entropy.py
@@ -1,24 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def clean_ribozyme(raw):
+    drop_chars = ['5','P','O','H','3','\'','-','\n']
+    for x in drop_chars:
+        print("Here is a x: ", x)
+        raw = raw.replace(x, '')
+    return raw
 
 
 path = "data/lincoln_joyce/ribozyme.txt"
 
 
 with open(path, 'r') as file:
-    data = file.read()
+    raw_data = file.read()
 
 
-# 
+cleaned_data     = clean_ribozyme(raw_data)
+nucleotide_list  = list(cleaned_data)
+
+plt.hist(list(nucleotide_list))
+plt.show()
+
+
 # a_count = data.count('a')
 # g_count = data.count('g')
 # c_count = data.count('c')
 # u_count = data.count('u')
-# 
-
-def clean_ribozyme(raw):
-    drop_chars = ['5','P','O','H','3','\'','-','\n']
-    for x in drop_chars:
-        data = data.replace(x, '')
-
-
-print(type(data))
-print(data)

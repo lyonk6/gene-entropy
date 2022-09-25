858b86f51fabfe003e32b15981eb573d006ce52b
Ken Lyon <fatstrat389@gmail.com>
2022-09-24 18:37:29 -0700

    cleaning up.

--- rna_entropy.py
+++ rna_entropy.py
@@ -5,51 +5,44 @@ import math
 def clean_ribozyme(raw):
     drop_chars = ['5','P','O','H','3','\'','-','\n']
     for x in drop_chars:
-        print("Here is a x: ", x)
+        #print("Here is a x: ", x)
         raw = raw.replace(x, '')
     return raw
-
 
 def get_lincoln_joyce_data():
     path = "data/lincoln_joyce/ribozyme.txt"
     with open(path, 'r') as file:
         return file.read()
 
-
-
-input_data       = get_lincoln_joyce_data()
-cleaned_data     = clean_ribozyme(input_data)
-nucleotide_list  = list(cleaned_data)
-
-
-plt.hist(list(nucleotide_list))
-plt.show()
-
-naive_probabilities = {
-    "a": 0.25,
-    "g": 0.25,
-    "c": 0.25,
-    "u": 0.25
-}
-
-short_nucleotide_list = ['a','g','c']
-
-# Calculate entropy: 
-# for each character in a message, look up it's probability 
-# value, then plug in to the following function:
-
-# probability * math.log(probability, Base)
-sum = 0
-for x in short_nucleotide_list:
-    sum += naive_probabilities[x] * math.log(naive_probabilities[x], 2)
-sum = 0 - sum
-print(sum)
-
-def calculate_entropy(message, character_probabilities):
+# Calculate entropy: For each character in a message, look up it's
+# probability value, then plug in to the following function:
+#     prob * math.log(prob, base)
+def calculate_entropy(message, probability_map):
     sum = 0
     for x in message:
-        sum += character_probabilities[x] * math.log(character_probabilities[x], 2)
+        if x in probability_map:
+            sum += probability_map[x] * math.log(probability_map[x], 2)
+        else:
+            print("Error. Character not allowed: ", x)
     return 0 - sum
+
+
+
+if __name__ == '__main__':
+    print("Start")
+
+    input_data       = get_lincoln_joyce_data()
+    cleaned_data     = clean_ribozyme(input_data)
+    nucleotide_list  = list(cleaned_data)
+
+    plt.hist(list(nucleotide_list))
+    plt.show()
+
+    naive_probabilities = {"a": 0.25, "g": 0.25, "c": 0.25, "u": 0.25}
+    short_nucleotide_list = ['a','g','c']
+
+    entropy =  calculate_entropy(short_nucleotide_list, naive_probabilities)
+    print("Entropy: ", entropy)
 
 # for x in nucleotide_list:
 # a_count = data.count('a')
